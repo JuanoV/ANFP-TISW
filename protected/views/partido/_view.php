d4d9badd09bf1d8479cefb1224453f2b36d79163
Ruben <rbntejeda@hotmail.com>
2015-07-01 19:48:27 -0300

    cambios tablas partido css

--- protected/views/partido/_view.php
+++ protected/views/partido/_view.php
@@ -6,33 +6,38 @@ $equipo1=Equipo::model()->findByPk($part[0]->PART_equCorrel);
 $equipo2=Equipo::model()->findByPk($part[1]->PART_equCorrel);
 
 ?>
-
-<div class="view">
 <div class="parent">
-    <div class="left">
-        <h3><center><b><?php echo $equipo1->EQU_nombre ?></b><br></h3>
-        <?php  
-        echo '<img src="'.Yii::app()->request->baseUrl.'/images/'.$equipo1->EQU_correl.'.png">';
-        ?></center>
-        <!-- <br />
-        <b> <?php //echo $part->PART_posicion ?></b>
-        <br /> -->
-    </div>
-    <div class="center">
-        <center>
-        <b><h1><?php echo $part[0]->PART_goles ?></b>
-        <!-- <b><?php //echo CHtml::encode($data->getAttributeLabel('PART_goles')); ?>:</b>
-        <?php //echo CHtml::encode($data->PART_goles); ?> -->
-        <?php  echo '<img src="'.Yii::app()->request->baseUrl.'/images/versus.ico">';?>
-        <?php echo $part[1]->PART_goles ?></center>
-        <center><h1><?php echo $data->PAR_programacion ?></center>
-    </div>
-    <div class="right">
-        <h3><center><b><?php echo $equipo2->EQU_nombre ?></b><br></h3>
-        <?php  
-        echo '<img src="'.Yii::app()->request->baseUrl.'/images/'.$equipo2->EQU_correl.'.png">';
-        ?></center>
-    </div>
-    <div style="clear:both;"></div>
-</div>
+    <table style="width:100%">
+    <tr>
+        <td>
+             <center><h3><b><?php echo $equipo1->EQU_nombre ?></b><br></h3></center>
+        </td>
+        <td>
+
+        </td>
+        <td>
+             <center><h3><b><?php echo $equipo2->EQU_nombre ?></b><br></h3></center>
+        </td>
+    </tr>
+    <tr>
+        <td width="40%">
+            <center>
+                <?php  echo '<img src="'.Yii::app()->request->baseUrl.'/images/'.$equipo1->EQU_correl.'.png" width="50%">';?>
+            </center>
+        </td>
+        <td width="20%">
+            <center>
+                <b><h1><?php echo $part[0]->PART_goles ?>
+                <?php  echo '<img src="'.Yii::app()->request->baseUrl.'/images/versus.ico" width="50%">';?>
+                <?php echo $part[1]->PART_goles ?></h1></b>
+            </center>
+            <center><h1><?php echo $data->PAR_programacion ?></h1></center>
+        </td>
+        <td width="40%">
+            <center>
+                <?php  echo '<img src="'.Yii::app()->request->baseUrl.'/images/'.$equipo2->EQU_correl.'.png" width="50%"';?>
+            </center>
+        </td>
+    </tr>
+    </table>
 </div>
