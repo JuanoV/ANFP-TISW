53b405f6e134e527b9d356b9868a43b97d421a23
JuanoV <jveliz@alumnos.ubiobio.cl>
2015-07-02 17:38:20 -0300

    aa

--- protected/config/main.php
+++ protected/config/main.php
@@ -158,7 +158,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=anfp',
+			'connectionString' => 'mysql:host=localhost;dbname=tisw_grupo5',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
